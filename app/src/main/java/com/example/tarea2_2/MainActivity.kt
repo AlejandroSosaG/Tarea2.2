b1ebe8b3b37f2dba7eb74836e36b67cbf39e4701
AlejandroSosaG <alejandro.sosa@iesnervion.es>
2023-10-05 14:32:12 +0200

    nulo

--- app/src/main/java/com/example/tarea2_2/MainActivity.kt
+++ app/src/main/java/com/example/tarea2_2/MainActivity.kt
@@ -9,19 +9,25 @@ import com.example.tarea2_2.databinding.ActivityMainBinding
 import com.example.tarea2_2.databinding.CalculadoraBinding
 
 class MainActivity : AppCompatActivity() {
-    var usuario: String = ""
-    var contraseña: String = ""
+    var usuario: String = "ale"
+    var contraseña: String = "1234"
     var sol: Double = 0.0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val main = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(main.root)
         val calculadora = CalculadoraBinding.inflate(layoutInflater)
+        setContentView(main.root)
         main.acceder.setOnClickListener(object : View.OnClickListener{
             override fun onClick(v: View?){
-                usuario = main.completaUsuario.text.toString()
-                setContentView(calculadora.root)
-                val tostada = Toast.makeText(applicationContext, "Estoy en calculadora", Toast.LENGTH_SHORT).show()
+                if (usuario == main.completaUsuario.text.toString() && contraseña == main.completaContraseA.text.toString()) {
+                    setContentView(calculadora.root)
+                    val tostada = Toast.makeText(
+                        applicationContext,
+                        "Estoy en calculadora",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
             }
         })
         calculadora.Vuelta.setOnClickListener(object : View.OnClickListener{
@@ -30,13 +36,46 @@ class MainActivity : AppCompatActivity() {
                 val main = Toast.makeText(applicationContext, "Estoy en main", Toast.LENGTH_SHORT).show()
             }
         })
-        var uno: Double = calculadora.numero1.text.toString().toDouble()
-        var dos: Double = calculadora.numero2.text.toString().toDouble()
         calculadora.mas.setOnClickListener(object : View.OnClickListener{
             override fun onClick(p0: View?) {
-                sol = uno + dos
-                calculadora.resultado.text = sol.toString()
+                sol = calculadora.numero1.text.toString().toDouble() + calculadora.numero2.text.toString().toDouble()
+                calculadora.sol.text = sol.toString()
             }
         })
+        calculadora.menos.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(p0: View?) {
+                sol = calculadora.numero1.text.toString().toDouble() - calculadora.numero2.text.toString().toDouble()
+                calculadora.sol.text = sol.toString()
+            }
+        })
+        calculadora.por.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(p0: View?) {
+                sol = calculadora.numero1.text.toString().toDouble() * calculadora.numero2.text.toString().toDouble()
+                calculadora.sol.text = sol.toString()
+            }
+        })
+        calculadora.entre.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(p0: View?) {
+                if (comprobarNulos() == true)
+                    sol = calculadora.numero1.text.toString().toDouble() / calculadora.numero2.text.toString().toDouble()
+                calculadora.sol.text = sol.toString()
+            }
+        })
+
+    }
+
+    private fun comprobarNulos(): Boolean {
+        val calculadora = CalculadoraBinding.inflate(layoutInflater)
+        var num1 = calculadora.numero1.text.toString().toDouble()
+        var num2 = calculadora.numero2.text.toString().toDouble()
+        var nulo: Boolean = true
+        if (num1 == null || num1 == 0.0){
+            num1 = 0.0
+            nulo = false
+        }else if (num2 == null || num2 == 0.0){
+            num2 = 0.0
+            nulo = false
+        }
+        return nulo
     }
 }
